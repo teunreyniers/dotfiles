803d431b7f0a55be6e440c5c0d7057f3fdd0f55c
Teun Reyniers <teun.reyniers@hotmail.be>
2025-05-06 22:01:38 +0200

    First commit!

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo pacman -S git tree stow
+
+
+git config --global user.email "teun.reyniers@hotmail.be"
+git config --global user.name "Teun Reyniers"
+git config --global init.defaultBranch main
