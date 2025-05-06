6ff8fb396b9e5bc58945cbb405de8966766bc653
teunreyniers <teun.reyniers@hotmail.be>
2025-05-06 23:12:03 +0200

    more items

---
+++ README.md
@@ -0,0 +1,12 @@
+# dotfiles
+
+
+Show tree overview of files
+```
+tree -a -I '.git'
+```
+
+```
+stow
+```
+
--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-sudo pacman -S git tree stow
+# Install basic packages
+sudo pacman -S git tree stow less zip unzip zsh firefox
 
-
+# Configure git
 git config --global user.email "teun.reyniers@hotmail.be"
-git config --global user.name "Teun Reyniers"
+git config --global user.name "teunreyniers"
 git config --global init.defaultBranch main
+
+
+# Terminal
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
