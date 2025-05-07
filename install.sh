681384010e68416bf36b7f8262a089f58cf04de3
teunreyniers <teun.reyniers@hotmail.be>
2025-05-07 13:30:52 +0200

    CI

--- install.sh
+++ install.sh
@@ -1,36 +1,40 @@
+#!/bin/bash
 # Install basic packages
-sudo pacman -S --no-confirm git tree stow less zip unzip zsh firefox tff-jetbrains-mono ttf-jetbrains-mono-nerd jq
+sudo pacman -S --noconfirm --needed git tree stow less zip unzip zsh firefox ttf-jetbrains-mono ttf-jetbrains-mono-nerd jq waybar base-devel
 sudo pacman -S --noconfirm --needed gcc make git ripgrep fd unzip neovim
 
 # Configure git
-set_git_username() {
-    current_username=$(git config --global user.name)
-    if [ -z "$current_username" ]; then
-        read -p "Enter your Git username: " username
-        git config --global user.name "$username"
-        echo "Git username set to: $username"
-    else
-        echo "Git username is already set to: $current_username"
-    fi
-}
-
-set_git_email() {
-    current_email=$(git config --global user.email)
-    if [ -z "$current_email" ]; then
-        read -p "Enter your Git email: " email
-        git config --global user.email "$email"
-        echo "Git email set to: $email"
-    else
-        echo "Git email is already set to: $current_email"
-    fi
-}
-
-# Main script execution
-set_git_username
-set_git_email
+current_username=$(git config --global user.name)
+if [ -z "$current_username" ]; then
+    read -p "Enter your Git username: " username
+    git config --global user.name "$username"
+fi
+current_email=$(git config --global user.email)
+if [ -z "$current_email" ]; then
+    read -p "Enter your Git email: " email
+    git config --global user.email "$email"
+fi
 git config --global init.defaultBranch main
 
 
 # Terminal
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
+# AUR - yay
+mkdir $HOME/git
+git clone https://aur.archlinux.org/yay.git $HOME/git/yay
+cd $HOME/git/yay
+makepkg -si
+
+# Bluetooth
+if dmesg | grep -i bluetooth > /dev/null; then
+    sudo pacman -S --noconfirm --needed bluez bluez-utils
+    sudo systemctl start bluetooth.service
+    sudo systemctl enable bluetooth.service
+    yay -S --noconfirm rofi-bluetooth-git
+fi
+
+# Audio
+sudo pacman -S --noconfirm --needed helvum
